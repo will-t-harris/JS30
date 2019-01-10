b5313c3bba7ea6b12413333cd32eefe74e872c25
wtharris89 <wtharris89@gmail.com>
2019-01-10 14:41:51 -0700

    setup scrub function and event listners, then do calculation to relate offsetX, offsetWidth and video.duration. finally set currentTime to be equal to the newly calculated scrubTime

--- 11 - Custom Video Player HTML5/scripts.js
+++ 11 - Custom Video Player HTML5/scripts.js
@@ -34,6 +34,12 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
+
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
@@ -46,3 +52,5 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
