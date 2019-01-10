77675cf1c75e5c4ea6b7602dd766be869e9fbae7
wtharris89 <wtharris89@gmail.com>
2019-01-10 13:53:36 -0700

    more typos

--- 11 - Custom Video Player HTML5/scripts.js
+++ 11 - Custom Video Player HTML5/scripts.js
@@ -1,12 +1,20 @@
 // Get out elements
 const player = document.querySelector('.player');
-const video = player.querySelector('viewer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('progress-filled');
-const toggle = player.querySelector('toggle');
+const progressBar = player.querySelector('.progress-filled');
+const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 // Build out functions
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
 
 // Hook up the event listeners
