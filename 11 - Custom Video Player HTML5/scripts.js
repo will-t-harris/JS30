23a1ca8fe993ac6781465b58c07a974b517bfd2d
wtharris89 <wtharris89@gmail.com>
2019-01-10 14:29:28 -0700

    finished handleRangeUpdate function and linked with change and mousemove event listeners

--- 11 - Custom Video Player HTML5/scripts.js
+++ 11 - Custom Video Player HTML5/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress-filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
 function togglePlay() {
@@ -22,7 +22,11 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(this.dataset);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
 }
 
 
@@ -34,3 +38,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
