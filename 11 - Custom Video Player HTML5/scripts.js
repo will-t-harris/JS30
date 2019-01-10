3da8c4e82d38c486ac245efe25d5427c709dfe36
wtharris89 <wtharris89@gmail.com>
2019-01-10 14:17:02 -0700

    setup event listeners for skip buttons

--- 11 - Custom Video Player HTML5/scripts.js
+++ 11 - Custom Video Player HTML5/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress-filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 // Build out functions
@@ -17,14 +17,20 @@ function togglePlay() {
 }
 
 function updateButton() {
-    console.log('Update the button');
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset);
 }
 
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
-video.addEventListener('paused', updateButton);
+video.addEventListener('pause', updateButton);
 
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
